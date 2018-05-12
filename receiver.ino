b77d7b1aa90ab652fb5d997043aa1d04f7827cac
Di Hou <34199798+DiHouyey@users.noreply.github.com>
2018-05-11 21:19:55 -0400

    Add files via upload

---
+++ receiver.ino
@@ -0,0 +1,34 @@
+String coor;
+void setup() {
+
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+  while (Serial.available()) {
+    delay(2);  
+    char c = Serial.read();
+    coor += c;
+  }
+
+  if (coor.length() >0) {
+    Serial.println(coor);
+
+    coor="";
+  } 
+  //Transform string coor into 4 int values which are x1,x2,y1,y2
+  //String coor="123456789000";
+//String a=coor.substring(0,3);
+// String b=coor.substring(3,6);
+//String c=coor.substring(6,9);
+//String d=coor.substring(9,12);
+//int x1=a.toInt();
+//int x2=b.toInt();
+//int y1=c.toInt();
+//int y2=d.toInt();
+  
+
+  }
+
---
+++ transXbee.ino
@@ -0,0 +1,68 @@
+#include <Wire.h>
+String coor = "";
+int x =0;
+void setup() {
+  // Define the LED pin as Output
+ Serial.begin(9600);
+  // Start the I2C Bus as Slave on address 9
+  Wire.begin(9); 
+  // Attach a function to trigger when something is received.
+  Wire.onReceive(receiveEvent);
+}
+void receiveEvent(int hs) {
+  
+  x = Wire.read();  
+  coor=String (x);
+ String st="99999900";
+ st=st+coor.substring(0,1);
+  st=st+"00";
+  st=st+coor.substring(1,2);
+
+
+  
+  Serial.println(st);
+  
+  }
+
+void loop() {
+  
+ 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
